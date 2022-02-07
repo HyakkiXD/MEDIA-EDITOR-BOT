73f54e10727589a42cb2602eea12d9d8224350e5
Percy <percy@railway.app>
2022-02-07 14:57:52 +0000

    Initial Commit

---
+++ config.py
@@ -0,0 +1,15 @@
+import os
+
+class Config(object):
+    
+    DOWNLOAD_LOCATION = "./DOWNLOADS"
+
+    TG_BOT_TOKEN = "5213804428:AAErePs3253-4lH2M3o-GsETRGsuq6GGuGo"
+
+    APP_ID = "6784106"
+
+    API_HASH = "b7060e7c133c5b999e3405ec0a4e95f5"
+
+    AUTH_USERS = "1380685014"
+  
+    
